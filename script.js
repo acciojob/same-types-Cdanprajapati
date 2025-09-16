47d7d8b62105ef8173eb5e94d5fef54a0cff7ca5
Cdan Prajapati <114227227+Cdanprajapati@users.noreply.github.com>
2025-09-16 21:44:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ function isSameType(value1, value2) {
     return true;
   }
 
+	 if (Number.isNaN(value1) || Number.isNaN(value2)) {
+    return false; // only one is NaN
+  }
+	
   // Compare their types
   return typeof value1 === typeof value2;
 }
